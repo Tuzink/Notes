e2314a60b61032d5fc194220639f47352ab55f3c
= <=>
2025-08-18 22:34:10 -0300

    Adicionei a página de Anotações

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string caminho = Path.Combine(FileSystem.AppDataDirectory,"arquivo");
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SalvarBtn_Clicked(object sender, EventArgs e)
+        {
+            string conteudo = CaixaEditor.Text;
+            File.WriteAllText(caminho, conteudo);
+
+        }
+
+        private void ApagarBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
